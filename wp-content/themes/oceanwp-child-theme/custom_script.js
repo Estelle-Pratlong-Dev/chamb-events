d7848ffe06a42110f40ca5ebcc8c21ca18c60e73
Estelle <pratlong.estelle@outlook.fr>
2026-02-20 23:37:35 +0100

    Backup Fairgrounds

---
+++ wp-content/themes/oceanwp-child-theme/custom_script.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $(document).ready(function () {
+
+
+        /********************** FOOTER *************************/
+        // $('#footer-bottom .date').html(new Date().getFullYear());
+    });
+}(jQuery));
